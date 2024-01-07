7bf492e805d5afd7b8973871712ab64aced52d7b
Edith Camacho <edith.teodocia@gmail.com>
2024-01-07 08:14:07 -0400

    N desplazar en el arreglo

---
+++ practices/Rotation.java
@@ -0,0 +1,23 @@
+package practicelogic.practices;
+
+import java.util.*;
+
+public class Rotation{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        int[] number = new int[10];
+        for(int i = 0; i < 10; i++){
+            System.out.printf("%d. Ingrese el numero: ", (i+1));
+            number[i] = sc.nextInt();
+        }
+        System.out.println("Ingrese el numero de veces que se rote: ");
+        int n = sc.nextInt(), i = 0;
+         int[] rotated = new int[10];
+        for (  i = 0; i < 10; i++) {
+            rotated[(i + n) % 10] = number[i];
+        }
+        System.out.println(Arrays.toString(rotated));    
+         
+    }
+}
